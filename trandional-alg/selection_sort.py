7c5b34a9f055b5d7134b845b1732e128c66e057a
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-06-03 21:28:35 +0800

    Create selection_sort.py

---
+++ trandional-alg/selection_sort.py
@@ -0,0 +1,10 @@
+def selection_sort(lst):
+    for i in range(len(lst)):
+        min_index = i
+        for j in range(i + 1, len(lst)):
+            # Update minimum index
+            if lst[j] < lst[min_index]:
+                min_index = j
+
+        lst[min_index], lst[i] = lst[i], lst[min_index]
+    return lst
