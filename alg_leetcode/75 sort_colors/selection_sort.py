e84010b21dc9e96ae65d6f75d8bca92158bd9c33
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-06-03 21:34:00 +0800

    Update selection_sort.py

--- alg_leetcode/75 sort_colors/selection_sort.py
+++ alg_leetcode/75 sort_colors/selection_sort.py
@@ -1,1 +1,14 @@
 
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            min_index = i
+            for j in range(i,len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            nums[i],nums[min_index] = nums[min_index],nums[i]
+        return nums
