b6b6720d3645172fe331c78758754dbacf799c43
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-06-07 18:34:06 +0800

    Update 181. Employees Earning More Than Their Managers.sql

--- sql/181. Employees Earning More Than Their Managers.sql
+++ sql/181. Employees Earning More Than Their Managers.sql
@@ -6,3 +6,10 @@ LEFT JOIN
 FROM Employee) AS m
 ON e.managerId = m.id
 WHERE salary > m_salary;
+
+# Second Solution 
+SELECT name AS Employee
+FROM (SELECT id AS m_id,salary AS m_salary
+FROM Employee) AS manager_salary,
+Employee
+WHERE managerId  = m_id AND salary > m_salary;
