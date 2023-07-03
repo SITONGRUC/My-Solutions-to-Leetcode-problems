af7a0c366412299bc728e06017f59855d892a649
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-03 22:57:50 +0800

    Update and rename a.sql to page_group.sql

---
+++ sql/example_code/page_group.sql
@@ -0,0 +1,11 @@
+
+WITH Events AS (
+  SELECT DISTINCT Event
+  FROM Summer_Medals)
+  
+SELECT
+  --- Split up the distinct events into 111 unique groups
+  Event,
+  NTILE(111) OVER (ORDER BY Event ASC) AS Page
+FROM Events
+ORDER BY Event ASC;
