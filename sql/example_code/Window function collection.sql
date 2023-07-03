c93fea5c811c904a9c64d813983d810d2d3d7a2a
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-03 23:08:30 +0800

    Update Window function collection.sql

--- sql/example_code/Window function collection.sql
+++ sql/example_code/Window function collection.sql
@@ -6,4 +6,17 @@ LAST_VALUES()
 RANK()
 DENSE_RANK()
 ROW_NUMBER()
+NTILE(111)
 ORDER BY & PARTITION BY could be used sperately. 
+
+
+WITH Events AS (
+  SELECT DISTINCT Event
+  FROM Summer_Medals)
+  
+SELECT
+  --- Split up the distinct events into 111 unique groups
+  Event,
+  NTILE(111) OVER (ORDER BY Event ASC) AS Page
+FROM Events
+ORDER BY Event ASC;
