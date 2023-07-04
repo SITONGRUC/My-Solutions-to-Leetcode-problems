3c08a96d28bc69238a14999780680c1eecf4ca5d
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-05 01:46:22 +0800

    Create 1571. Warehouse Manager.sql

---
+++ sql/1571. Warehouse Manager.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT 
+name AS warehouse_name,
+SUM(space.space*w.units) AS volume 
+FROM 
+Warehouse AS w
+LEFT JOIN
+(SELECT 
+product_id ,
+Width*Length*Height AS space
+FROM Products) AS space
+ON w.product_id = space.product_id
+GROUP BY warehouse_name;
