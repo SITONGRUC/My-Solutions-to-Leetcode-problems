e4055b8d46f23e7e28b9c0e9e6628c78000af833
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-04 14:44:01 -0500

    Create 2329. Product Sales Analysis V.sql

---
+++ sql/2329. Product Sales Analysis V.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+user_id ,
+SUM(quantity*price) AS spending
+FROM Sales  AS s
+LEFT JOIN Product AS p
+ON s.product_id = p.product_id
+GROUP BY user_id 
+ORDER BY spending DESC,user_id ASC;
