5f846934e69a19aeb292f8d70a4da2e09211e911
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-07 21:45:14 +0800

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- sql/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ sql/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -10,3 +10,12 @@ Transactions AS t
 ON t.visit_id = v.visit_id
 GROUP BY  customer_id 
 HAVING count_no_trans != 0;
+
+
+
+SELECT
+customer_id,
+COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT visit_id FROM Transactions )
+GROUP BY customer_id;
