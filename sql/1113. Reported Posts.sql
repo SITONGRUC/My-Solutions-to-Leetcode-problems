03db0d293abc0e1e955a334e4b51e09efc1f9651
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-08 12:26:56 +0800

    Update 1113. Reported Posts.sql

--- sql/1113. Reported Posts.sql
+++ sql/1113. Reported Posts.sql
@@ -5,3 +5,12 @@ FROM Actions
 WHERE action_date = DATE_ADD('2019-07-05', INTERVAL -1 DAY) 
 GROUP BY extra
 HAVING extra IS NOT NULL AND extra != 'love';
+
+
+select 
+  extra as report_reason
+  ,count(distinct post_id) as report_count
+from Actions
+where action_date = '2019-07-04'
+  and action = 'report'
+group by extra
