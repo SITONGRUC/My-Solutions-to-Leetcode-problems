b7aa94bae4ea1f9682e829917db202c98f709a86
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-09 13:07:20 -0500

    Create 1264. Page Recommendations.sql

---
+++ sql/1264. Page Recommendations.sql
@@ -0,0 +1,7 @@
+SELECT 
+DISTINCT page_id AS recommended_page
+FROM Likes
+WHERE (user_id IN (SELECT user1_id FROM 
+Friendship WHERE user2_id = 1) OR user_id IN (SELECT user2_id FROM 
+Friendship WHERE user1_id = 1)  ) AND
+page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1 )
