424897d72ea9f55af22f5d389107ea8eead612d7
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-10 11:52:27 -0500

    Create 1445. Apples & Oranges.sql

---
+++ sql/1445. Apples & Oranges.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT
+sale_date,
+SUM(CASE WHEN  fruit = 'apples' THEN sold_num END)-SUM(CASE WHEN  fruit = 'oranges' THEN sold_num END) AS diff 
+FROM Sales
+GROUP BY sale_date  
+ORDER BY sale_date ASC;
