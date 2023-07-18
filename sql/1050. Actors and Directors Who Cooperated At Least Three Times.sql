034e7204af8e52a251cd2c393775fd2ec1056b6e
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-17 20:40:39 -0500

    Update 1050. Actors and Directors Who Cooperated At Least Three Times.sql

--- sql/1050. Actors and Directors Who Cooperated At Least Three Times.sql
+++ sql/1050. Actors and Directors Who Cooperated At Least Three Times.sql
@@ -10,3 +10,12 @@ COUNT(timestamp) AS count
 FROM ActorDirector
 GROUP BY actor_id,director_id
 HAVING count >=3) AS c
+
+# Write your MySQL query statement below
+SELECT
+DISTINCT
+actor_id ,
+director_id
+FROM ActorDirector
+GROUP BY actor_id,director_id
+HAVING COUNT(timestamp) >= 3;
