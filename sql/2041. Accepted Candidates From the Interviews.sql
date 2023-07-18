e7492194fba4134654d06c7f1f4817b9da8ca5ed
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-07-18 07:25:10 -0500

    Create 2041. Accepted Candidates From the Interviews.sql

---
+++ sql/2041. Accepted Candidates From the Interviews.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT
+candidate_id 
+FROM Candidates 
+WHERE years_of_exp >=2
+AND 
+interview_id  IN (
+SELECT interview_id 
+FROM Rounds 
+GROUP BY interview_id 
+HAVING SUM(score) >15) ;
