2ecd1462e3bf38aa5ea25603970566ac15f10e0d
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-02 19:08:55 -0500

    Update 183. Customers Who Never Order.sql

--- sql/183. Customers Who Never Order.sql
+++ sql/183. Customers Who Never Order.sql
@@ -4,3 +4,11 @@ FROM Customers
 LEFT JOIN Orders
 ON Orders.customerId = Customers.id
 WHERE  Orders.id IS NULL;
+
+# Write your MySQL query statement below
+SELECT
+name AS Customers
+FROM Customers
+WHERE Customers.id NOT IN (
+  SELECT customerId FROM Orders
+)
