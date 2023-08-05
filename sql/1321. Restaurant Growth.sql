fcd98cc4da39dccec7f7f4a4c1f5aa3a65648f5f
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-05 10:36:41 -0500

    Update 1321. Restaurant Growth.sql

--- sql/1321. Restaurant Growth.sql
+++ sql/1321. Restaurant Growth.sql
@@ -16,3 +16,27 @@ WHERE visited_on >= DATE_ADD(
   (SELECT MIN(visited_on) FROM Customer) , INTERVAL 6 DAY
 )
 ORDER BY visited_on ASC;
+
+
+# Write your MySQL query statement below
+SELECT
+    visited_on,
+    (
+        SELECT SUM(amount)
+        FROM customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount,
+    ROUND(
+        (
+            SELECT SUM(amount) / 7
+            FROM customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        ),
+        2
+    ) AS average_amount
+FROM customer c
+WHERE visited_on >= (
+        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+        FROM customer
+    )
+GROUP BY visited_on;
