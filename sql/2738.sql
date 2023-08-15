1f1850a7b12b0cd460a91ad5742f771012573cd1
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-15 09:29:29 -0500

    Create 2738.sql

---
+++ sql/2738.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+'bull' AS word,
+SUM(CASE WHEN content LIKE '% bull %' THEN 1 ELSE 0 END) AS count
+FROM Files
+UNION ALL 
+SELECT
+'bear' AS word,
+SUM(CASE WHEN content LIKE '% bear %' THEN 1 ELSE 0 END) AS count
+FROM Files
