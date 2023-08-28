3d95023c26620daf54cbab433e100b2b6e44eff8
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-28 08:54:18 -0500

    Update 570. Managers with at Least 5 Direct Reports.sql

--- sql/570. Managers with at Least 5 Direct Reports.sql
+++ sql/570. Managers with at Least 5 Direct Reports.sql
@@ -13,3 +13,19 @@ Employee AS mentee
 ON mentee.managerId = mentor.id
 GROUP BY mentor.id
 HAVING cnt >=5) AS res
+
+#____________dd________
+
+  
+SELECT 
+name
+FROM Employee
+WHERE id IN 
+(
+SELECT 
+managerId
+FROM Employee
+WHERE managerId IS NOT NULL 
+GROUP BY managerId
+HAVING COUNT(DISTINCT id) >= 5
+)
