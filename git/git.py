4a3b1c8205c4226859594036431121e9d42ed3de
SITONGRUC <tigonggeig@gmail.com>
2023-08-28 14:02:44 -0500

    add the useful command line in github

---
+++ git/git.py
@@ -0,0 +1,55 @@
+pwd
+
+ls
+
+mkdir demo
+
+cd "./demo"
+
+git init ( if you just pull from GitHub you don't need this line)
+
+ls -a ( could show hidden files0
+
+ls .git( you could also show the files in .git)
+
+Git status
+
+git add hello.txt
+
+git commit
+
+git log --all --graph --decorate
+
+git checkout 9e8d705cf( could check out different commit, move head from upset down)
+
+
+git checkout master
+
+(Output:Switched to branch 'master')
+
+git branch
+
+git branch -vv
+
+git branch cat(make a new branch)
+
+git diff
+
+git checkout master( if you would merge to master first switch back to master)
+
+git diff dog ( check the difference between two version)
+
+Git clone ( could clone rep from github)
+
+(Example:  git clone git@github.com:SITONGRUC/CFA-example.git )
+
+git pull (update your local rep)
+
+git push origin main ( update your remote rep) 
+
+
+
+
+
+
+
