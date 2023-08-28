13ae4a8ccd64514751a9ebd257b0f2e3ec9975ee
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-28 08:45:39 -0500

    Update 184. Department Highest Salary.sql

--- sql/184. Department Highest Salary.sql
+++ sql/184. Department Highest Salary.sql
@@ -14,3 +14,18 @@ ON e.salary = top.salary AND e.departmentId = top.departmentId
 LEFT JOIN 
 Department AS d
 ON d.id = e.departmentId;
+
+#--------------------------------------------
+
+SELECT 
+d.name AS Department,
+res.name AS Employee,
+res.salary AS salary
+FROM 
+(SELECT 
+*,
+RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+FROM Employee) AS res
+LEFT JOIN Department AS d
+ON d.id = res.departmentId
+WHERE rnk = 1
