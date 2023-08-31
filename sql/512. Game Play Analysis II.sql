9bfb5faf184ccd1ce62530d387329ef083d49903
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-08-31 11:56:34 -0500

    Update 512. Game Play Analysis II.sql

--- sql/512. Game Play Analysis II.sql
+++ sql/512. Game Play Analysis II.sql
@@ -10,3 +10,14 @@ FROM Activity
 GROUP BY player_id) AS res
 LEFT JOIN Activity AS a
 ON a.event_date = res.event_date AND a.player_id = res.player_id
+#_________________________________________________________________________________--
+
+SELECT 
+player_id ,
+device_id 
+FROM (
+SELECT
+*,
+RANK() OVER(PARTITION BY player_id  ORDER BY event_date  ASC ) AS rnk
+FROM Activity ) AS res
+WHERE rnk = 1
