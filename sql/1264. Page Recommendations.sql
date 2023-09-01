da754f588aece523a1360b0636bfdc1a95ec806c
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-01 10:15:57 -0500

    Update 1264. Page Recommendations.sql

--- sql/1264. Page Recommendations.sql
+++ sql/1264. Page Recommendations.sql
@@ -5,3 +5,18 @@ WHERE (user_id IN (SELECT user1_id FROM
 Friendship WHERE user2_id = 1) OR user_id IN (SELECT user2_id FROM 
 Friendship WHERE user1_id = 1)  ) AND
 page_id NOT IN (SELECT page_id FROM Likes WHERE user_id = 1 )
+
+SELECT
+DISTINCT page_id AS recommended_page
+FROM 
+(SELECT user1_id AS first, user2_id AS second FROM Friendship
+UNION ALL 
+SELECT user2_id AS first, user1_id AS second FROM Friendship) AS res
+LEFT JOIN Likes AS l
+ON l.user_id = res.second
+WHERE first IN (1)
+AND 
+page_id NOT IN (
+    SELECT page_id FROM Likes WHERE user_id = 1
+)
+AND page_id IS NOT NULL 
