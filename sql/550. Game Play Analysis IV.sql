b1d4e9056ab5299bb91887819cca4e528106695e
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-06 08:51:15 -0500

    Update 550. Game Play Analysis IV.sql

--- sql/550. Game Play Analysis IV.sql
+++ sql/550. Game Play Analysis IV.sql
@@ -15,3 +15,22 @@ player_id,
 event_date AS first_day
 FROM Activity) AS res,
 Activity AS act) AS fraction
+
+
+
+
+WITH 
+sec AS (
+       SELECT
+    player_id,
+    DATE_ADD(MIN(event_date), INTERVAL 1 DAY) AS sec
+    FROM Activity
+    GROUP BY player_id
+)
+
+SELECT
+CASE WHEN ROUND(COUNT(DISTINCT c.player_id)/COUNT(DISTINCT a.player_id),2) IS NOT NULL THEN ROUND(COUNT(DISTINCT c.player_id)/COUNT(DISTINCT a.player_id),2) ELSE 0 END AS fraction
+FROM Activity AS a
+LEFT JOIN sec AS c
+ON a.player_id = c.player_id AND a.event_date = c.sec
+
