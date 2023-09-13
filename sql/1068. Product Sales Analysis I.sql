90afa3798c671687fe14b4aef209574b88ca880d
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-13 08:16:00 -0500

    Update 1068. Product Sales Analysis I.sql

--- sql/1068. Product Sales Analysis I.sql
+++ sql/1068. Product Sales Analysis I.sql
@@ -7,3 +7,10 @@ FROM Product AS p
 INNER JOIN 
 Sales AS s 
 ON  p.product_id = s.product_id 
+
+SELECT
+p.product_name,
+s.year,
+s.price
+FROM Sales AS s,Product AS p
+WHERE p.product_id = s.product_id
