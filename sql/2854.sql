ac88e7245b27bd7b7b9332643284b69caa09c44d
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-17 08:34:06 -0500

    Update 2854.sql

--- sql/2854.sql
+++ sql/2854.sql
@@ -36,3 +36,54 @@ LEFT JOIN Steps AS s
 ON s.user_id = date_list.user_id AND s.steps_date = date_list.steps_date) AS result) AS result2
 WHERE CNT >=3
 ORDER BY user_id ASC,steps_date ASC
+
+#_________________________________________________________________________
+
+# Write your MySQL query statement below
+WITH cet AS (
+SELECT 
+s.user_id,
+s.steps_date
+FROM Steps AS s
+LEFT JOIN 
+(
+    SELECT
+    user_id, DATE_ADD(steps_date,INTERVAL -1 DAY ) AS steps_date
+    FROM Steps
+) AS lg
+ON lg.user_id = s.user_id AND lg.steps_date = s.steps_date
+LEFT JOIN 
+(
+    SELECT
+    user_id, DATE_ADD(steps_date,INTERVAL 1 DAY ) AS steps_date
+    FROM Steps
+) AS ld
+ON ld.user_id = s.user_id AND ld.steps_date = s.steps_date
+WHERE ld.user_id IS NOT NULL AND lg.user_id IS NOT  NULL )
+
+
+SELECT
+user_id,
+steps_date,
+ROUND(rolling_average,2) AS rolling_average
+FROM (
+SELECT
+*,
+AVG(steps_count) OVER(PARTITION BY user_id ORDER BY steps_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS rolling_average,
+ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY steps_date) AS cnt
+FROM (
+SELECT
+DISTINCT 
+s.user_id,
+s.steps_date,
+s.steps_count
+FROM 
+(SELECT user_id,steps_date FROM cet
+UNION 
+SELECT user_id,DATE_ADD(steps_date,INTERVAL -1 DAY) FROM cet
+UNION 
+SELECT user_id,DATE_ADD(steps_date,INTERVAL 1 DAY) FROM cet) AS date_list
+LEFT JOIN Steps AS s
+ON s.user_id = date_list.user_id AND s.steps_date = date_list.steps_date) AS result) AS result2
+WHERE CNT >=3
+ORDER BY user_id ASC,steps_date ASC
