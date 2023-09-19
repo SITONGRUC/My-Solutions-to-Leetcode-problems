b0bf1548058b92fb0de550e9ebffa91ef4afaafe
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-19 07:58:31 -0500

    Update 2853. Highest Salaries Difference.sql

--- sql/2853. Highest Salaries Difference.sql
+++ sql/2853. Highest Salaries Difference.sql
@@ -6,3 +6,20 @@ WHERE department = 'Engineering')-(SELECT
 MAX(salary) AS v
 FROM Salaries
 WHERE department = 'Marketing')) AS salary_difference
+
+
+# Write your MySQL query statement below
+WITH cet
+AS (
+SELECT
+*,
+RANK() OVER(PARTITION BY department ORDER BY salary DESC) AS rnk
+FROM Salaries)
+
+SELECT
+DISTINCT 
+ABS(c1.salary - c2.salary) AS salary_difference
+FROM cet AS c1
+LEFT JOIN cet AS c2
+ON c1.rnk = c2.rnk
+WHERE c1.rnk = 1 AND c2.rnk = 1 AND c1.department != c2.department
