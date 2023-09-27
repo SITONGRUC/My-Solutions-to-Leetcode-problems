d59adaae1309c2968e1d492e8259e7b5fc1020cb
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-27 08:05:37 -0500

    Update 1571. Warehouse Manager.sql

--- sql/1571. Warehouse Manager.sql
+++ sql/1571. Warehouse Manager.sql
@@ -11,3 +11,17 @@ Width*Length*Height AS space
 FROM Products) AS space
 ON w.product_id = space.product_id
 GROUP BY warehouse_name;
+
+
+SELECT
+w.name AS warehouse_name ,
+SUM(w.units*space.space) AS volume
+FROM Warehouse  AS w
+LEFT JOIN 
+(
+    SELECT 
+product_id ,
+Width * Length  * Height AS space
+FROM Products ) AS space
+ON w.product_id    = space.product_id 
+GROUP BY w.name
