14e442eb90deb83e60c89e1dd183eaa6d70e79dd
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2023-09-27 08:10:55 -0500

    Update 2066. Account Balance.sql

--- sql/2066. Account Balance.sql
+++ sql/2066. Account Balance.sql
@@ -10,3 +10,12 @@ day ,
 CASE WHEN  type ='Deposit' THEN amount 
 ELSE -amount END AS amount 
 FROM Transactions) AS res
+
+
+
+SELECT
+account_id,
+day,
+SUM(CASE WHEN type = 'Deposit' THEN amount ELSE 0-amount END) OVER(PARTITION BY account_id ORDER BY day ASC) AS balance
+FROM Transactions
+ORDER BY account_id ASC,day ASC
