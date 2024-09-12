3688e2031dab9643df41c74c26df4009370e1e11
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-09-11 22:43:43 -0500

    Update 175. Combine Two Tables.sql

--- sql/175. Combine Two Tables.sql
+++ sql/175. Combine Two Tables.sql
@@ -3,3 +3,9 @@ SELECT p.firstName,p.lastName,city,state
 FROM Person AS p
 LEFT JOIN Address AS a
 ON p.personId = a.personId;
+
+
+SELECT p.firstName,p.lastName,a.city,a.state FROM person AS p
+LEFT JOIN 
+address AS a 
+ON  p.personid = a.personid;
