668e6ef7a102c6fb27522ac330fc949df332268e
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-09-23 20:19:20 -0500

    Update 607. Sales Person.sql

--- sql/607. Sales Person.sql
+++ sql/607. Sales Person.sql
@@ -6,3 +6,15 @@ WHERE sales_id NOT IN
 WHERE com_id = 
 (SELECT com_id FROM Company WHERE name = 'RED')) 
 #Read the need carefully
+
+
+# Write your MySQL query statement below
+SELECT sp.name FROM SalesPerson AS sp
+WHERE sp.sales_id NOT IN (
+SELECT o.sales_id FROM Orders AS o
+LEFT JOIN Company AS c
+ON c.com_id = o.com_id
+WHERE c.name = 'RED');
+
+
+
