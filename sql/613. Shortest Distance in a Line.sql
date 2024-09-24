570e3114b8141f1106b6adf35d5e8a5083c43cbe
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-09-23 20:25:14 -0500

    Update 613. Shortest Distance in a Line.sql

--- sql/613. Shortest Distance in a Line.sql
+++ sql/613. Shortest Distance in a Line.sql
@@ -5,3 +5,10 @@ FROM Point AS p2
 CROSS JOIN 
 Point AS p1
 WHERE p1.x != p2.x;
+
+# There is no difference
+
+SELECT MIN(ABS(p1.x-p2.x)) AS shortest
+FROM Point AS p1,Point AS p2
+WHERE p1.x != p2.x;
+
