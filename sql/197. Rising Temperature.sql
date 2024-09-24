cc98f4f038853cb9fbf6730b75236462fdc9f9fb
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-09-23 19:45:20 -0500

    Update 197. Rising Temperature.sql

--- sql/197. Rising Temperature.sql
+++ sql/197. Rising Temperature.sql
@@ -13,3 +13,10 @@ FROM Weather
 ) AS b 
 ON a.yesterday = b.today
 WHERE today_tem>yesterday_tem
+
+# Write your MySQL query statement below
+SELECT w.id FROM Weather AS w
+LEFT JOIN (
+SELECT *,DATE_ADD(recordDate, INTERVAL 1 DAY) AS date FROM Weather AS w) AS last_day
+ON w.recordDate = last_day.date
+WHERE w.temperature -last_day.temperature >0
