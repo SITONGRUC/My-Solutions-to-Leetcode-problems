98d9541d5e2977e76fdf0d577763a1feb3ed8dcd
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-09-23 19:54:52 -0500

    Update 586. Customer Placing the Largest Number of Orders.sql

--- sql/586. Customer Placing the Largest Number of Orders.sql
+++ sql/586. Customer Placing the Largest Number of Orders.sql
@@ -6,3 +6,14 @@ GROUP BY customer_number
 ORDER BY count DESC
 LIMIT 1
 ) AS count_result
+
+
+# Write your MySQL query statement bel
+SELECT customer_number  FROM  Orders AS o
+GROUP BY customer_number
+HAVING COUNT(order_number) = (
+SELECT MAX(cnt) FROM (
+SELECT customer_number,COUNT(order_number) AS cnt FROM  Orders AS o
+GROUP BY customer_number) AS cot ) ;
+
+
