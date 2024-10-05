b85b1d203ffa3ab69e48293c3228c7c2949ead48
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-04 21:52:30 -0500

    Update 1587. Bank Account Summary II.sql

--- sql/1587. Bank Account Summary II.sql
+++ sql/1587. Bank Account Summary II.sql
@@ -8,3 +8,14 @@ LEFT JOIN Transactions AS t
 ON t.account = u.account
 GROUP BY name
 HAVING balance > 10000;
+
+
+
+# Write your MySQL query statement below
+
+SELECT 
+u.name,
+(SELECT SUM(t.amount) FROM transactions AS t WHERE t.account = u.account ) AS balance
+FROM users AS u
+GROUP BY u.name
+HAVING balance > 10000;
