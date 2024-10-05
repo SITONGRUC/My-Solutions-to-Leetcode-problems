e2a29308a3a0710241a594ef77e8e8947a76381d
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-05 09:31:27 -0500

    Update 1731. The Number of Employees Which Report to Each Employee.sql

--- sql/1731. The Number of Employees Which Report to Each Employee.sql
+++ sql/1731. The Number of Employees Which Report to Each Employee.sql
@@ -14,3 +14,15 @@ HAVING reports_to IS NOT NULL) AS p
 LEFT JOIN  Employees AS e
 ON p.employee_id = e.employee_id
 ORDER BY employee_id ASC;
+
+
+SELECT 
+e.employee_id,
+e.name,
+(SELECT COUNT(e1.employee_id) FROM employees AS e1 WHERE e1.reports_to = e.employee_id) AS  reports_count,
+ROUND((SELECT AVG(e2.age) FROM employees AS e2 WHERE e2.reports_to = e.employee_id),0) AS average_age
+FROM employees AS e
+GROUP BY e.employee_id
+HAVING average_age IS NOT NULL AND reports_count >0
+ORDER BY e.employee_id;
+
