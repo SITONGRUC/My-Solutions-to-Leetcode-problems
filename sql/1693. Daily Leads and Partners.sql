fe123f164146bdc5392cdeda19548040f62a5eb1
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-05 09:19:19 -0500

    Update 1693. Daily Leads and Partners.sql

--- sql/1693. Daily Leads and Partners.sql
+++ sql/1693. Daily Leads and Partners.sql
@@ -19,3 +19,12 @@ COUNT(DISTINCT partner_id)  AS unique_partners
 FROM DailySales
 GROUP BY date_id,make_name) AS b
 ON b.date_id = a.date_id AND a.make_name = b.make_name;
+
+
+SELECT
+d.date_id,
+d.make_name,
+(SELECT COUNT(DISTINCT lead_id) FROM DailySales AS d1 WHERE d1.date_id = d.date_id AND d1.make_name = d.make_name) AS unique_leads,
+(SELECT COUNT(DISTINCT partner_id) FROM DailySales AS d2 WHERE d2.date_id = d.date_id AND d2.make_name = d.make_name) AS unique_partners
+FROM DailySales AS d
+GROUP BY d.date_id,d.make_name;
