f424455e6858cb376258e29e7f2c8721e9975a36
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-05 09:07:53 -0500

    Update 1661 Average Time of Process per Machine.sql

--- sql/1661 Average Time of Process per Machine.sql
+++ sql/1661 Average Time of Process per Machine.sql
@@ -7,3 +7,12 @@ machine_id,
 MAX(timestamp) OVER(PARTITION BY machine_id,process_id)-MIN(timestamp) OVER(PARTITION BY machine_id,process_id) AS  duration_time
 FROM Activity) AS t
 GROUP BY machine_id;
+
+
+
+SELECT 
+a.machine_id,
+ROUND(AVG((SELECT timestamp FROM Activity AS end WHERE end.activity_type = 'end' AND a.machine_id = end.machine_id AND a.process_id = end.process_id )-
+(SELECT timestamp FROM Activity AS start WHERE start.activity_type = 'start' AND a.machine_id = start.machine_id AND a.process_id = start.process_id )),3) AS processing_time
+FROM Activity AS a
+GROUP BY a.machine_id;
