c891bf7d41b16950136e8042797af7896af822b1
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-06 09:06:23 -0500

    Update 2686. Immediate Food Delivery III.sql

--- sql/2686. Immediate Food Delivery III.sql
+++ sql/2686. Immediate Food Delivery III.sql
@@ -9,3 +9,12 @@ CASE WHEN order_date =customer_pref_delivery_date THEN 1 ELSE 0 END AS judge
 FROM Delivery) AS res
 GROUP BY order_date
 ORDER BY order_date ASC
+
+SELECT 
+order_date,
+ROUND(100*SUM(
+    CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END 
+)/COUNT(*),2) AS 'immediate_percentage'
+FROM Delivery AS d
+GROUP BY order_date
+ORDER BY order_date ASC;
