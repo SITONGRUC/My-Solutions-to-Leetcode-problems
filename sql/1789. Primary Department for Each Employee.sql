eab434b7140248acd996a8ad70f633ecfc477dac
SITONGRUC <72329233+SITONGRUC@users.noreply.github.com>
2024-10-05 19:38:17 -0500

    Update 1789. Primary Department for Each Employee.sql

--- sql/1789. Primary Department for Each Employee.sql
+++ sql/1789. Primary Department for Each Employee.sql
@@ -11,3 +11,20 @@ primary_flag,
 ELSE 0 END ) AS cnt
 FROM Employee) AS data
 WHERE data.cnt= 1 OR (data.cnt= 0 AND primary_flag = 'Y' );
+
+
+SELECT 
+e.employee_id,
+e.department_id
+FROM Employee AS e
+WHERE e.primary_flag = 'Y'
+UNION 
+SELECT 
+e1.employee_id,
+e1.department_id
+FROM Employee AS e1
+GROUP BY e1.employee_id
+HAVING COUNT(e1.employee_id) = 1;
+
+
+
